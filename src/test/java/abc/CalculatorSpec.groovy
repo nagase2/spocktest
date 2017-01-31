cefcb55010af6db0d3fef0fba88773c3ae32cc30
yoshi-PC\yoshi <nagase2@gmail.com>
2017-02-01 01:18:06 +0900

    修正

--- src/test/java/abc/CalculatorSpec.groovy
+++ src/test/java/abc/CalculatorSpec.groovy
@@ -8,22 +8,55 @@ import spock.lang.Unroll
 
 class CalculatorSpec extends Specification {
 
-  @Unroll
-  def add() {
-	given:
+	@Unroll
+	def addoperatorTest() {
+		given:
+		def calc = new Calculator()
+
+		when:
+		def result = calc.add(a, b)
+
+		then:
+		result == expected
+
+		where:
+		a  | b  | expected
+		1  | 1  | 2
+		0  | 0  | 0
+		1  | 0  | 1
+		0  | 1  | 1
+	}
+	@Unroll("#a ‚©‚ç #b ‚ðˆø‚¢‚½‚ç #exp")
+	def "ˆø‚«ŽZ‚ÌƒeƒXƒg"() {
+		given:
+		def calc = new Calculator()
+		
+		when:
+		def result =calc.extract(a,b)
+		then: 
+		result == exp
+		where:
+		a|b|exp
+		1|1|0
+		2|1|1
+		1|5|-4
+		1|1|0
+		1|1|0
+		
+	}
+	@Unroll("#a should have length #b")
+	def "name length"() {
+	  given:
 	  def calc = new Calculator()
-
-	when:
-	  def result = calc.add(a, b)
-
-	then:
-	  result == expected
-
-	where:
-	  a  | b  | expected
-	  1  | 1  | 2
-	  0  | 0  | 0
-	  1  | 0  | 1
-	  0  | 1  | 1
-  }
+	//  when:
+	 // def result = calc.extract(a,b)
+	  
+	  expect:
+	  a-b == calc.extract(a,b)
+   
+	  where:
+	  a << [1,2,3]
+	  b << [4, 5, 6]
+	}
+	
 }
